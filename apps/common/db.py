ce241da9f523e984ab0ab1287717b6fce9321644
hss3771 <hss6642@naver.com>
2025-12-29 13:09:53 +0900

    update db.py

--- apps/common/db.py
+++ apps/common/db.py
@@ -3,9 +3,9 @@ from sqlalchemy.orm import sessionmaker
 
 user_id = 'web_user'
 pw = 'pass'
-host = 'localhost'
+host = '192.168.0.34'
 port = 3306
-db = 'mydb'
+db = 'trendscope'
 
 url = f'mysql+pymysql://{user_id}:{pw}@{host}:{port}/{db}'
 engine = create_engine(url,echo=False,pool_size=1)
