aa1bf777ccbad179ab6741225112f4c8cba68be1
hss3771 <hss6642@naver.com>
2025-12-30 12:38:31 +0900

    update db.py

--- apps/common/db.py
+++ apps/common/db.py
@@ -21,7 +21,7 @@ port = 3306
 db = 'trendscope'
 
 url = f'mysql+pymysql://{user_id}:{pw}@{host}:{port}/{db}'
-engine = create_engine(url,echo=False,
+engine = create_engine(url,echo=True,
                        pool_size=10,
                        max_overflow=20,
                        pool_timeout=30,
@@ -119,9 +119,73 @@ def get_user_role(user_id):
         return result
 
 # 아이디찾기
+def find_user_id(name, email):
+    db = get_db()
+    info = {"name": name, "email": email}
+    sql = text("""SELECT user_id
+                   FROM users
+                   WHERE name = :name AND email = :email""")
+    result = None
+    try:
+        result = db.execute(sql, info).mappings().fetchone()
+        if result:
+            result = result['user_id']
+            result = {"state": 1, "user_id": result}
+        else:
+            result = {"state": 0, "user_id": "일치하는 정보가 없습니다."}
+    except Exception as e:
+        print(f'Find user ID error: {e}')
+        result = {"state": 2, "user_id": "에러 발생"}
+    finally:
+        db.close()
+        return result
+    
 # 비밀번호찾기
+def find_user_pw(user_id, name, email):
+    db = get_db()
+    info = {"user_id": user_id, "name": name, "email": email}
+    sql = text("""SELECT EXISTS (
+                   SELECT 1
+                   FROM users
+                   WHERE user_id = :user_id AND name = :name AND email = :email
+                   ) AS is_valid""")
+    result = None
+    try:
+        result = db.execute(sql, info).mappings().fetchone()
+        if result['is_valid']:
+            result = 1 # 성공
+        else:
+            result = 0 # 일치하는 정보 없음
+    except Exception as e:
+        print(f'Find user password error: {e}')
+        result = 2 # 에러 발생
+    finally:
+        db.close()
+        return {"state": result}
+
 # 비밀번호 변경
-# 메인페이지
+def change_user_pw(user_id, new_pw):
+    db = get_db()
+    info = {"user_id": user_id, "password_hash": hashlib.sha256(new_pw.encode()).hexdigest()}
+    sql = text("""UPDATE users
+                   SET password_hash = :password_hash
+                   WHERE user_id = :user_id""")
+    result = None
+    try:
+        r = db.execute(sql, info)
+        if r.rowcount:
+            result = 1 #"비밀번호 변경 완료"
+        else:
+            result = 0 #"비밀번호 변경 실패"
+    except Exception as e:
+        print(f'Change user password error: {e}')
+        db.rollback()
+        result = 2 #"비밀번호 변경 실패"
+    finally:
+        db.commit()
+        db.close()
+        return {"state": result}
+
 # 회원가입
 def register_user(info):
     db = get_db()
@@ -129,22 +193,83 @@ def register_user(info):
                    VALUES (:user_id, :password_hash, :email, :name, :birthday, :phone, :eco_state, :gender)""")
     result = None
     try:
-        db.execute(sql, info)
-        db.commit()
+        r = db.execute(sql, info)
+        if r.rowcount:
+            sql = text("INSERT INTO user_roles (user_id, role_id) VALUES (:user_id, '1')")
+            db.execute(sql, {'user_id': info['user_id']})
         result = "등록 완료"
     except Exception as e:
         print(f'Registration error: {e}')
         db.rollback()
         result = "등록 실패"
     finally:
+        db.commit()
         db.close()
         return {"message": result}
-
+    
 # 중복 확인
-# 회원정보 수정
+def check_duplicate_user_id(user_id):
+    db = get_db()
+    sql = text("""SELECT EXISTS (
+                   SELECT 1
+                   FROM users
+                   WHERE user_id = :user_id
+                   ) AS is_exist""")
+    result = None
+    try:
+        result = db.execute(sql, {'user_id': user_id}).mappings().fetchone()
+        if result['is_exist']:
+            result = 0
+        else:
+            result = 1
+    except Exception as e:
+        print(f'Check duplicate user ID error: {e}')
+        result = 2
+    finally:
+        db.close()
+        return result
 
+# 비밀번호 확인
+def check_user_pw(user_id, pw):
+    db = get_db()
+    sql = text("""SELECT EXISTS (
+            SELECT 1
+            FROM users
+            WHERE user_id = :user_id
+            AND password_hash = :password_hash
+            ) AS is_valid"""
+        )
+    info = {"user_id": user_id, "password_hash": pw}
+    result = None
+    try:
+        result = db.execute(sql, info).mappings().fetchone()
+        if result['is_valid']:
+            result = 1 # 성공
+        else:
+            result = 0 # 실패
+    except Exception as e:
+        print(f'Check duplicate user ID error: {e}')
+        result = 2 # 에러
+    finally:
+        db.close()
+        return result
+
+# 회원정보 불러오기 #미완
+def get_user_info(user_id):
+    db = get_db()
+    result = None
+    info = {"user_id" : user_id}
+    sql = text("""
+                SELECT name, gender, birthday, phone, email, eco_state
+               FROM users
+               where user_id = :user_id
+                """)
+
+
+# # 회원정보 수정
+# def update
 if __name__ == "__main__":
-    # 임시 유저 등록
+    #임시 유저 등록
     # db.py확인용
     # print(register_user(information))
     # 실사용 코드
@@ -171,4 +296,20 @@ if __name__ == "__main__":
     # 실사용 코드
     # db.get_user_role("siwoo")
     # 결과 => user/admin
+    ###########################################################################
+    # 임시 중복 확인
+    # db.py확인용
+    # print(check_duplicate_user_id("gkstmdtn"))
+    # print(check_duplicate_user_id("gks"))
+    #실사용 코드
+    # db.check_duplicate_user_id(user_id)
+    # 결과 => 0(중복), 1(중복아님), 2(에러)
+    ###########################################################################
+    # 임시 중복 확인
+    # db.py확인용
+    # print(check_user_pw(information["user_id"],information["password_hash"])) => 1
+    # print(check_user_pw("gkstmdtn", "gkstmdtn")) => 0
+    # 실사용 코드
+    # db.check_user_pw(user_id)
+    # 결과 => 0(실패), 1(통과), 2(에러)
     pass
