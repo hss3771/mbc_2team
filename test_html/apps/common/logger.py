8974c3813ef32b59d2ec10261e8bcc73a0dc1f7a
Jiyoungb7 <zz248@naver.com>
2026-01-02 17:40:47 +0900

    Delete test_html/apps directory

--- test_html/apps/common/logger.py
+++
@@ -1,9 +0,0 @@
-import logging
-class Logger:
-    logging.basicConfig(
-        level=logging.INFO,
-        format='%(levelname)s: [%(filename)s] %(message)s - %(asctime)s',
-        datefmt='%Y-%m-%d %H:%M:%S',
-    )
-    def get_logger(self,name):
-        return logging.getLogger(name)
